2e774a463f8522357c9b37cbe151e1a852d7e992
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-04-04 17:20:07 +0300

    firstAssigments,pdf file of the drill, the local database

---
+++ FirstAssigment/ex1.py
@@ -0,0 +1,24 @@
+# =============================================================================
+#
+#    Program: Basic Sql 
+#    Author: Yaniv Gabay
+#    Date Created: 2024-04-04
+#
+#
+#    Description:
+#      Answer to the 31 questions given at [Assigment 1](https://mowgli.hac.ac.il/pluginfile.php/1622023/mod_resource/content/2/SQL_targil_1.pdf)         
+#      Each question is a query
+#    Requirments:
+#      pip install sqlalchemy pandas
+#      World.db3 local db file
+# =============================================================================
+
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+class DatabaseHandler:
+    def __init(self,db_url):
+        """
+        
+        """
